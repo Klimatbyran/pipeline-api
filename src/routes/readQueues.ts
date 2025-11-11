506341a6b625c26cb0ca955d119e7badc27d23ff
eli6 <elin.fritiofsson@protonmail.com>
2025-11-11 12:37:43 +0100

    support for runOnly via the pipeline api

--- src/routes/readQueues.ts
+++ src/routes/readQueues.ts
@@ -88,7 +88,7 @@ export async function readQueuesRoute(app: FastifyInstance) {
       if (!resolvedName) {
         return reply.status(400).send({ error: `Unknown queue '${name}'. Valid queues: ${Object.values(QUEUE_NAMES).join(', ')}` });
       }
-      const { urls, autoApprove, forceReindex, threadId, replaceAllEmissions } = request.body as any;
+      const { urls, autoApprove, forceReindex, threadId, replaceAllEmissions, runOnly } = request.body as any;
      
       // Resolve threadId: accept provided threadId/runId or generate a new one
       const providedOrCreatedThreadId = threadId || `run_${Date.now()}_${Math.random().toString(36).slice(2,8)}`;
@@ -100,13 +100,14 @@ export async function readQueuesRoute(app: FastifyInstance) {
           autoApprove: !!autoApprove,
           forceReindex: !!forceReindex,
           replaceAllEmissions: !!replaceAllEmissions,
+          runOnly: runOnly,
         },
         'Enqueue request received'
       );
       const queueService = await QueueService.getQueueService();
       const addedJobs: BaseJob[] = [];
       for(const url of urls) {
-        const addedJob = await queueService.addJob(resolvedName, url, autoApprove, { forceReindex, threadId: providedOrCreatedThreadId, replaceAllEmissions });
+        const addedJob = await queueService.addJob(resolvedName, url, autoApprove, { forceReindex, threadId: providedOrCreatedThreadId, replaceAllEmissions, runOnly });
         addedJobs.push(addedJob);
       }
       return reply.send(addedJobs);
